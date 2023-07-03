07373728d697f2b762fd8f81f2b29a59dbdcd33b
unknown <denis9159597861@yandex.ru>
2023-07-03 04:23:10 +0300

    3 июня. робот едет от команды с компьютера

--- test_mqtt2.py
+++ test_mqtt2.py
@@ -1,15 +1,96 @@
-# Рабочая версия кода для Распберри!!!
+﻿# Рабочая версия кода для Распберри!!!
 import time
 import random #generate id of client
 from paho.mqtt import client as mqtt_client
 from operator import length_hint
 
+import smbus
+import os
+import copy
+bus = smbus.SMBus(1)
+SLAVE_ADDRESS = 0x20
+
 class robot_path_node:
 	def __init__(self, action1, counter1):
 		self.action=action1
 		self.counter=counter1
+
+def writenumber(num):
+	bus.write_byte(SLAVE_ADDRESS, int(num))
+def write_array(a, b, c, d):
+	bus.write_i2c_block_data(SLAVE_ADDRESS, a, [b, c, d])
+def readnumber():
+	num=int(bus.read_byte(SLAVE_ADDRESS))   #|bus.read_byte(SLAVE_ADDRESS)#read_byte
+	return num
+
+
+def mode_1(robot_action):
+	sz_path=len(robot_action)
+	result = [] # результат работы mode1
+
+	writenumber(sz_path) #send robot_action to robot
+	for i in range(0, sz_path):
+		writenumber(robot_action[i].action)
+		writenumber(robot_action[i].counter)
+		print(robot_action[i].action, "  ", robot_action[i].counter)
+
+	status=25  #по умолчанию
+	j_recv=-1
+	number_recv_message=-1
+	while(status==25):
+		recv_message=readnumber()
+		print("recv_message = ", recv_message)
+		if(recv_message==255):
+			number_recv_message=0
+		if(number_recv_message==1):
+			j_recv=recv_message
+		if(number_recv_message==2):
+			status=recv_message
+			number_recv_message=-1
+		if(number_recv_message!=-1):
+			number_recv_message=(number_recv_message+1)%3
+
+		if(status!=25):
+			print("status = ", status)
+			print("j_recv = ", j_recv)
+		if(status<(sz_path-1)): #status - номер элемента массива robot_action, который был последним выполнен успешно
+			num_path=0
+			print("j_recv = ", j_recv)
+			for i in range(0, status): #status-1
+				num_path=num_path+robot_action[i].counter 
+			num_path=num_path+j_recv
+
+			result.append(0)
+			result.append(num_path)   #path[num_path])
+			result.append(num_path+1)   #path[num_path + 1])
+			print("num_path = ", num_path)
+			print("path[num]=", path[num_path])
+			print("barrier")
+			break
+		if(status==(sz_path-1)):
+			print("success")
+			result.append(1)
+			result.append(-1) #path[len(path)-1])
+			result.append(-1) #path[len(path)-1])
+			break
+		time.sleep(0.1)
+	return(result)
+
+def robot_trip(robot_action):
+	result = [] # результат работы robot_trip
+	result=mode_1(robot_action, path)
+	if(result[0]==0):
+		print("barrier!")
+		#вызов result[0]=mode_2()
+	if(result[0]==1):
+		print("success!")
+	return(result)
+
+
         
 robot_action=[]
+#path=[]
+#path=[0, 2, 1, 2, 0] #вместо этой строки path должен строиться сам
 action_tmp=-1           #принятое значение поля action, которое еще не добавлено в robot_action
 counter_tmp=-1          #принятое значение поля counter, которое еще не добавлено в robot_action
 recv_counter=0          #счетчик приятых сообщений при приеме массива robot_action
@@ -88,6 +169,7 @@ def on_message(client, userdata, msg):
 def mqtt_communication(): # может вызываться неограничеенное чсло раз
     global array_is_received
     global robot_action
+    global path
     client = connect_mqtt()
     client.loop_start()
     client.subscribe(topic)
@@ -104,11 +186,11 @@ def mqtt_communication(): # может вызываться неограниче
         
     #вместо этого блока должна быть функция robot_trip
     print(" ") #robot go!
-    time.sleep(5)
+    #time.sleep(5)
+    result_trip=robot_trip(robot_action)
     print("robot is work!")
     
-    
-    result_trip=[0, 2, 3]
+    #result_trip=[1, -1, -1]
     
     # результат поездки робота
     answer=str(result_trip[0])+" " + str(result_trip[1]) + " " + str(result_trip[2]) + " " #пробел в конце обязателен, иначе распарсер на складе не будет работать!
@@ -137,7 +219,7 @@ def mqtt_communication(): # может вызываться неограниче
     client.loop_stop() 
     
     
-for i in range(0, 4):
+for i in range(0, 1):
     mqtt_communication()
 
 
